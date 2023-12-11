f0fc9d274f7ee25f112e9e734f4a4afaae6eb604
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-11 21:09:26 +0100

    Added windows 8.1 support

---
+++ notifier/os_notifiers/windows_legacy.py
@@ -0,0 +1,21 @@
+import pathlib
+import os
+import subprocess
+import tempfile
+import uuid
+import codecs
+
+from loguru import logger
+from ._base import BaseNotifier
+
+from win10toast import ToastNotifier
+
+
+class WindowsLegacyNotifier(BaseNotifier):
+    """Main Notification System for Windows (8.1). Uses win10toast"""
+    def __init__(self, **kwargs):
+        self._notifier = ToastNotifier()
+        
+
+    def send_notification(self, notification_title, notification_subtitle, notification_icon, application_name, notification_audio, **kwargs,):
+        self._notifier.show_toast(notification_title, notification_subtitle, duration=10)
