fa6f9eb14de6d0d3af857107d913098e4768cc8e
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-11 21:38:05 +0100

    updated example

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
 from notifier import Notifier
 
-notifier = Notifier(override_windows_version_detection=True, override_windows_version="8.1")
+notifier = Notifier(override_windows_version_detection=True)
 notifier.send()
