12032e82abd14d3750603477e85d2f449e3a64f1
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-11 22:22:18 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -11,14 +11,13 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="notify_py",
+    name="pynotifier",
     version="0.3.38",
-    author="Mustafa Mohamed",
-    author_email="ms7mohamed@gmail.com",
-    description="Cross-platform desktop notification library for Python",
+    author="Juliandev02",
+    description="A simple Python library that simplifies the sending of desktop notifications!",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ms7m/notify-py",
+    url="https://github.com/Strawberry-Foundations/notifier",
     python_requires=">=3.6.0",
     packages=find_packages(
         exclude=["testing", "*.testing", "*.testing.*", "testing.*", "tests"]
@@ -28,7 +27,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    entry_points={"console_scripts": ["notifypy = notifypy.cli:entry"]},
+    entry_points={"console_scripts": ["notifier = notifier.cli:entry"]},
     include_package_data=True,
     install_requires=["loguru", "jeepney ; platform_system=='Linux'"],
 )
