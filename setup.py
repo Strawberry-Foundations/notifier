0a274959b1c29cf9488aeddcdc6e75c410d4c290
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-12 18:29:18 +0100

    updated version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r", encoding="utf8") as fh:
 
 setup(
     name="notifier.py",
-    version="0.4.2",
+    version="0.4.3",
     author="Juliandev02",
     description="A simple Python library that simplifies the sending of desktop notifications!",
     long_description=long_description,
